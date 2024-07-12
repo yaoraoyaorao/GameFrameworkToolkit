0898877e12120b35bd37bc741a32a829781b28c3
YaoRaoYaoRao <rencongyanggamestudio@gmail.com>
2024-07-12 17:19:09 +0800

    添加事件

---
+++ Event/Runtime/EventComponent.cs
@@ -0,0 +1,115 @@
+using System;
+using UnityEngine;
+
+namespace GameFramework.Toolkit.Runtime
+{
+    public class EventComponent : MonoBehaviour
+    {
+        private EventPool<GameEventArgs> m_EventPool;
+
+        /// <summary>
+        /// 获取事件处理函数的数量。
+        /// </summary>
+        public int EventHandlerCount
+        {
+            get
+            {
+                return m_EventPool.EventHandlerCount;
+            }
+        }
+
+        /// <summary>
+        /// 获取事件数量。
+        /// </summary>
+        public int EventCount
+        {
+            get
+            {
+                return m_EventPool.EventCount;
+            }
+        }
+
+        private void Start()
+        {
+            m_EventPool = new EventPool<GameEventArgs>(EventPoolMode.AllowNoHandler | EventPoolMode.AllowMultiHandler);
+        }
+
+        private void Update()
+        {
+            m_EventPool.Update();
+        }
+
+        private void OnDestroy()
+        {
+            m_EventPool.Shutdown();
+        }
+
+        /// <summary>
+        /// 获取事件处理函数的数量。
+        /// </summary>
+        /// <param name="id">事件类型编号。</param>
+        /// <returns>事件处理函数的数量。</returns>
+        public int Count(int id)
+        {
+            return m_EventPool.Count(id);
+        }
+
+        /// <summary>
+        /// 检查是否存在事件处理函数。
+        /// </summary>
+        /// <param name="id">事件类型编号。</param>
+        /// <param name="handler">要检查的事件处理函数。</param>
+        /// <returns>是否存在事件处理函数。</returns>
+        public bool Check(int id, EventHandler<GameEventArgs> handler)
+        {
+            return m_EventPool.Check(id, handler);
+        }
+
+        /// <summary>
+        /// 订阅事件处理函数。
+        /// </summary>
+        /// <param name="id">事件类型编号。</param>
+        /// <param name="handler">要订阅的事件处理函数。</param>
+        public void Subscribe(int id, EventHandler<GameEventArgs> handler)
+        {
+            m_EventPool.Subscribe(id, handler);
+        }
+        /// <summary>
+        /// 取消订阅事件处理函数。
+        /// </summary>
+        /// <param name="id">事件类型编号。</param>
+        /// <param name="handler">要取消订阅的事件处理函数。</param>
+        public void Unsubscribe(int id, EventHandler<GameEventArgs> handler)
+        {
+            m_EventPool.Unsubscribe(id, handler);
+        }
+        /// <summary>
+        /// 设置默认事件处理函数。
+        /// </summary>
+        /// <param name="handler">要设置的默认事件处理函数。</param>
+        public void SetDefaultHandler(EventHandler<GameEventArgs> handler)
+        {
+            m_EventPool.SetDefaultHandler(handler);
+        }
+
+        /// <summary>
+        /// 抛出事件，这个操作是线程安全的，即使不在主线程中抛出，也可保证在主线程中回调事件处理函数，但事件会在抛出后的下一帧分发。
+        /// </summary>
+        /// <param name="sender">事件源。</param>
+        /// <param name="e">事件参数。</param>
+        public void Fire(object sender, GameEventArgs e)
+        {
+            m_EventPool.Fire(sender, e);
+        }
+
+        /// <summary>
+        /// 抛出事件立即模式，这个操作不是线程安全的，事件会立刻分发。
+        /// </summary>
+        /// <param name="sender">事件源。</param>
+        /// <param name="e">事件参数。</param>
+        public void FireNow(object sender, GameEventArgs e)
+        {
+            m_EventPool.FireNow(sender, e);
+        }
+    }
+}
---
+++ Event/Runtime/GameEventArgs.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace GameFramework.Toolkit.Runtime
+{
+    public abstract class GameEventArgs : BaseEventArgs
+    {
+    }
+}
+
